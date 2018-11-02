026325632df55a63b7f638a00e578c6b9b12e50b
michaelrothbauer <38194512+michaelrothbauer@users.noreply.github.com>
2018-11-01 17:38:35 -0700

    Forms Commit
    Added author and genre forms

--- sample/controllers/genreController.js
+++ sample/controllers/genreController.js
@@ -1,4 +1,9 @@
 var Genre = require('../models/genre');
+var async = require('async');
+var Book = require('../models/book');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 
 // Display list of all Genres.
 exports.genre_list = function(req, res, next) {
@@ -14,19 +19,73 @@ exports.genre_list = function(req, res, next) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
-};
+exports.genre_detail = function(req, res, next) {
+
+    async.parallel({
+          genre: function(callback) {
+              Genre.findById(req.params.id)
+                .exec(callback)
+          },
+          genres_books: function(callback) {
+            Book.find({ 'genre': req.params.id },'title summary')
+            .exec(callback)
+          },
+      }, function(err, results) {
+          if (err) { return next(err); } // Error in API usage.
+          if (results.genre==null) { // No results.
+              var err = new Error('Genre not found');
+              err.status = 404;
+              return next(err);
+          }
+          // Successful, so render.
+          res.render('genre_detail', { title: 'Genre Detail',
+          genre: results.genre, genre_books: results.genres_books } );
+      });
+
+  };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = function(req, res, next) {
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
-// Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+// Handle Author create on POST.
+exports.genre_create_post = [
+
+    // Validate fields.
+    body('name').isLength({ min: 1 }).trim().withMessage('Genre must be specified.')
+        .isAlphanumeric().withMessage('Genre has non-alphanumeric characters.'),
+
+    // Sanitize fields.
+    sanitizeBody('name').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('genre_form', { title: 'Create Genre', genre: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Create an Author object with escaped and trimmed data.
+            var genre = new Genre(
+                {
+                    name: req.body.name,
+                });
+            genre.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(genre.url);
+            });
+        }
+    }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
